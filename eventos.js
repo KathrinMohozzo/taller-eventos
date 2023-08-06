c94d6b0f8f9502d5a4b22298dc28659babd4715b
KathrinMohozzo <140973758+KathrinMohozzo@users.noreply.github.com>
2023-08-06 20:49:17 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -5,10 +5,3 @@ const divElement = document.getElementById('btn');
 divElement.addEventListener('click', function() {
   alert('Hola! Soy el div');
 });
-
-const btnElement = document.getElementById('e');
-
-    btnElement.addEventListener('click', function(event) {
-      event.stopPropagation();
-      alert('Hola!');
-    });
